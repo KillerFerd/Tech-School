8c72a8e321a49fe10bba8c9a79dd0df412fad482
KillerFerd <91443717+KillerFerd@users.noreply.github.com>
2023-10-03 06:29:01 -0600

    Small Fix

--- routes/profesorcomite.routes.js
+++ routes/profesorcomite.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const profesorComiteController = require('../controllers/profesorComite.controller');
 
 // METODOS/ROUTES/ENDPOINTS: 
-router.get('/', profesorComiteController.getProfesorComite); 
+router.get('/', profesorComiteController.getProfesoresComites); 
 //router.get('/:profesorComiteId', profesorComiteController.getProfesorComiteById);
 //router.post('/', profesorComiteController.createProfesorComite);
 //router.put('/:profesorComiteId', profesorComiteController.updateProfesorComite);
